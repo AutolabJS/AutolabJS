228a895e606f8be69afe12733671afbfee7c8e93
rajat503 <rajat503@gmail.com>
2016-04-02 06:18:06 +0530

    untested potntially buggy code

--- main_server/main_server.js
+++ main_server/main_server.js
@@ -6,28 +6,11 @@ var bodyParser = require('body-parser');
 var fs = require('fs');
 var io = require('socket.io')(server);
 var config_details = require('./conf.json');
-var lab_config = require('./labs.json');
 var mysql = require('mysql');
 
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
-
-app.get('/connectionCheck', function (req,res) {
-  res.send(true);
-});
-
-app.post('/results', function(req, res){
-    var submission_id = req.body.id;
-    var scores = req.body.marks;
-    console.log(submission_id+ " "+scores);
-    res.send(true);
-});
-
-app.get('/', function (req,res) {
-    res.send('./public/index.html');
-});
-
 
 var load_balancer_hostname=config_details["load_balancer"].hostname;
 var load_balancer_port=config_details["load_balancer"].port;
@@ -38,38 +21,126 @@ var connection = mysql.createConnection(
 
 connection.connect();
 
-for(var i=0;i<lab_config["Labs"].length;i++)
+function initLabs()
 {
-  initDB(lab_config["Labs"][i].Lab_No,i);
+  lab_config = require('./labs.json');
+  for(var j=0;j<lab_config["Labs"].length;j++)
+  {
+    initScoreboard(lab_config["Labs"][j].Lab_No);
+  }
 }
 
-function initDB(lab_no,i) {
-  status=false
-  connection.query('SELECT * FROM Labs WHERE Lab_No=?', [lab_no] ,function(err, rows, fields) {
+function initScoreboard(lab_no) {
+  table_name='l'+lab_no;
+  connection.query('SELECT * FROM information_schema.tables WHERE table_schema = ? AND table_name = ? LIMIT 1', [config_details["database"].database,table_name] ,function(err, rows, fields) {
       if(rows.length==0)
       {
-        var q='CREATE TABLE l'+lab_no+'(name int)';
+        var q='CREATE TABLE l'+lab_no+'(id varchar(12), score int, time datetime)';
         connection.query(q, function(err, rows, fields) {
-            if(err) throw err;
-        });
-        q2="INSERT INTO Labs VALUES ("+lab_no+","+lab_config["Labs"][i].start_time+","+lab_config["Labs"][i].end_time+","+lab_config["Labs"][i].hard_deadline+","+lab_config["Labs"][i].penalty+","+lab_config["Labs"][i].testcases+","+status+")";
-        connection.query(q2,function(err,rows, fields) {
-            if (err) throw err;
-        });
-      }
-      else {
-        q3="UPDATE Labs SET start_time="+lab_config["Labs"][i].start_time+", end_time="+lab_config["Labs"][i].end_time+", hard_deadline="+lab_config["Labs"][i].hard_deadline+", penalty="+lab_config["Labs"][i].penalty+", test_cases="+lab_config["Labs"][i].testcases+", status="+status+" WHERE Lab_No="+lab_no;
-        // console.log(q3);
-        connection.query(q3,function(err,rows, fields) {
-            if (err) throw err;
         });
       }
   });
 }
 
+initLabs();
 server.listen(8080);
 console.log("Listening at 8080");
 
-// io.on('connection', function(socket) {
-//   console.log('A user has connected.');
-// });
+app.get('/', function (req,res) {
+  res.send('./public/index.html');
+});
+
+app.get('/reset', function (req,res) {
+  initLabs();
+  res.send(true);
+});
+
+app.post('/results', function(req, res){
+    var submission_id = req.body.id;
+    var scores = req.body.marks;
+    console.log(submission_id+ " "+scores);
+    res.send(true);
+    req.socket.emit('scores', req.scores);
+});
+
+io.on('connection', function(socket) {
+  lab_config = require('./labs.json');
+  var current_time= new Date();
+  labs_status=[];
+  for(var i=0;i<lab_config["Labs"].length;i++) {
+    start=new Date(lab_config["Labs"][i].start_year, lab_config["Labs"][i].start_month -1 ,lab_config["Labs"][i].start_date, lab_config["Labs"][i].start_hour,lab_config["Labs"][i].start_minute, 0,0);
+    end=new Date(lab_config["Labs"][i].end_year, lab_config["Labs"][i].end_month -1 ,lab_config["Labs"][i].end_date, lab_config["Labs"][i].end_hour,lab_config["Labs"][i].end_minute, 0,0);
+    hard=new Date(lab_config["Labs"][i].hard_year, lab_config["Labs"][i].hard_month -1 ,lab_config["Labs"][i].hard_date, lab_config["Labs"][i].hard_hour,lab_config["Labs"][i].hard_minute, 0,0);
+    var status = 0;
+    if(current_time-start > 0)
+    {
+      if(current_time - end < 0)
+      {
+        status=1;
+      }
+      else {
+        if(current_time - hard < 0)
+        {
+          status =2;
+        }
+      }
+    }
+
+    lab_x = {"Lab_No" :lab_config["Labs"][i], "status": status};
+    labs_status.push(lab_x);
+  }
+  socket.emit('labs_status', labs_status);
+
+  socket.on('submission', function(data) {
+    id_number=data[0];
+    lab_no=data[1];
+    current_time = new Date();
+    lab_config = require('./labs.json');
+    flag=0;
+    penalty=0;
+    for(var i=0;i<lab_config["Labs"].length;i++) {
+      if(lab_config["Labs"][i].Lab_No == lab_no)
+      {
+        start=new Date(lab_config["Labs"][i].start_year, lab_config["Labs"][i].start_month -1 ,lab_config["Labs"][i].start_date, lab_config["Labs"][i].start_hour,lab_config["Labs"][i].start_minute, 0,0);
+        end=new Date(lab_config["Labs"][i].end_year, lab_config["Labs"][i].end_month -1 ,lab_config["Labs"][i].end_date, lab_config["Labs"][i].end_hour,lab_config["Labs"][i].end_minute, 0,0);
+        hard=new Date(lab_config["Labs"][i].hard_year, lab_config["Labs"][i].hard_month -1 ,lab_config["Labs"][i].hard_date, lab_config["Labs"][i].hard_hour,lab_config["Labs"][i].hard_minute, 0,0);
+        flag=1;
+        break;
+      }
+    }
+    if(flag==1) {
+      var status = 0;
+      if(current_time-start > 0)
+      {
+        if(current_time - end < 0)
+        {
+          status=1;
+        }
+        else {
+          if(current_time - hard < 0)
+          {
+            status =2;
+            penalty=lab_config["Labs"][i].penalty;
+          }
+        }
+      }
+      body_json= {"id_no" :id_number, "Lab_No:" lab_no, "time:",current_time, "status": status, "penalty": penalty, "socket:", socket};
+      var body=JSON.stringify(body_json);
+      var request = new http.ClientRequest({
+        hostname: load_balancer_hostname,
+        port: load_balancer_port,
+        path: "/submit",
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Content-Length": Buffer.byteLength(body)
+        }
+      });
+      request.end(body);
+
+    }
+    else {
+      socket.emit("invalid", "Invalid Lab No");
+    }
+  });
+});
