584c09151980cc1bace6ade396cd7a1f48ef5e39
AnkshitJain <ankshitjain@gmail.com>
2017-03-12 11:29:46 +0530

    Reverted back
    removed sudo

--- test.sh
+++ test.sh
@@ -1,22 +1,23 @@
- #!/bin/bash
- 
+#!/bin/bash
+
 cd main_server
-sudo npm install
+npm install
 
 cd ../execution_nodes
-sudo npm install 
+npm install 
 
 cd ../load_balancer
-sudo npm install
+npm install
 
 sudo npm install -g jshint
- 
+
 cd ../main_server
- 
+
 jshint main_server.js
 jshint ../load_balancer/load_balancer.js;
 jshint ../execution_nodes/execute_node.js;
 
-sudo npm test
+npm test
+
 cd ../load_balancer;
-sudo npm test;
+npm test;
