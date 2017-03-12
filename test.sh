56c42f4e999dd8d2c2af22c8c3f8ad87b9ee9613
AnkshitJain <ankshitjain@gmail.com>
2017-03-12 14:05:09 +0530

    added eslint for the files
    Files are load_balancer.js, main_server.js, execute_nodes.js

--- test.sh
+++ test.sh
@@ -14,5 +14,8 @@ sudo npm install -g jshint
 cd ../main_server
 
 jshint main_server.js
+eslint main_server.js
 jshint ../load_balancer/load_balancer.js;
+eslint ../load_balancer/load_balancer.js;
 jshint ../execution_nodes/execute_node.js;
+eslint ../execution_nodes/execute_node.js;
