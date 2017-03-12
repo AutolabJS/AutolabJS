0ffe9c128138fde9cdb4f612dcb5ab8b18e489ec
AnkshitJain <ankshitjain@gmail.com>
2017-03-12 11:19:48 +0530

    Functionality: only to jshint

--- test.sh
+++ test.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-cd main_server
-sudo npm install
-
-cd ../execution_nodes
-sudo npm install 
-
-cd ../load_balancer
-sudo npm install
-
 sudo npm install -g jshint
 
 cd ../main_server
@@ -16,8 +7,3 @@ cd ../main_server
 jshint main_server.js
 jshint ../load_balancer/load_balancer.js;
 jshint ../execution_nodes/execute_node.js;
-
-sudo npm test
-
-cd ../load_balancer;
-sudo npm test;
