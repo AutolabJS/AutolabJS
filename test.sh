c0c25ea15c3e08802be0e9e1d921f91294ef27b0
AnkshitJain <ankshitjain@gmail.com>
2017-03-12 11:25:41 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,9 +1,22 @@
-#!/bin/bash
+ #!/bin/bash
+ 
+cd main_server
+sudo npm install
+
+cd ../execution_nodes
+sudo npm install 
+
+cd ../load_balancer
+sudo npm install
 
 sudo npm install -g jshint
-
+ 
 cd ../main_server
-
+ 
 jshint main_server.js
 jshint ../load_balancer/load_balancer.js;
 jshint ../execution_nodes/execute_node.js;
+
+sudo npm test
+cd ../load_balancer;
+sudo npm test;
