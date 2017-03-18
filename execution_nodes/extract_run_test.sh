d2d0e6afc8bc678210eed6c18ed4f7d47fe820ec
AnkshitJain <ankshitjain@gmail.com>
2017-03-18 20:03:48 +0530

    added labtest

--- execution_nodes/extract_run_test.sh
+++ execution_nodes/extract_run_test.sh
@@ -1,6 +1,11 @@
-mkdir ubmissions
+mkdir submissions
 cd submissions
 
-cp -rf ../../Test/* .
+mkdir 2015A7PS006G
+cd 2015A7PS006G
+
+mkdir labtest
+cd labtest
+cp -rf ../../../../Test/* .
 
 bash execute.sh python2  
