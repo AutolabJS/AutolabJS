5b4ddc6b54de01fb43389a7466c17b38f71fafed
AnkshitJain <ankshitjain@gmail.com>
2017-03-18 19:04:18 +0530

    extract_run_test added

--- execution_nodes/submissions/author_solution/c/add.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-
-int add(int a, int b, int c)
-{
-	return a+b+c;
-}
--- execution_nodes/submissions/test_cases/c/tests/Test1.cpp
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include "add.c"
-
-int test()
-{
-	if(add(3,4,5)==12) return 1;
-	else return 0;
-}
