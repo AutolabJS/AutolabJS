5dc6575337e2a73e8e4b031ad6367057c0e96344
prasadtalasila <tsrkp@goa.bits-pilani.ac.in>
2017-03-26 17:22:16 +0530

    revert submit.js to code available in commit 3191ada

--- tests/functional_tests/submit.js
+++ tests/functional_tests/submit.js
@@ -1,11 +1,10 @@
 var argv = require('minimist')(process.argv.slice(2));
+var Table = require('cli-table');
 
-var submit = function(id_no, current_lab, commit_hash, language) {
+var submit = function(id_no, current_lab, commit_hash) {
+	var commit_hash = "";
 	var socket = require('socket.io-client')('localhost'+':'+'9000');
-	var req = [id_no, current_lab, '', 'java'];
-	//var req = [id_no, current_lab, commit_hash, language];
-	console.log("\nRequest object: "+req);
-	socket.emit('submission', req);
+	socket.emit('submission', [id_no, current_lab , commit_hash, 'java']);
 	socket.on('invalid', function(data) {
 		console.log('Access Denied. Please try submitting again');
 		process.exit(0);
@@ -19,24 +18,16 @@ var submit = function(id_no, current_lab, commit_hash, language) {
 	socket.on('scores', function(data) {
 		total_score=0;
 		console.log('\nSubmission successful. Retreiving results');
-		delete data.socket;
-		console.log("\nResults object: %j", data);
+		console.log("\nevaluation object: %j", data);
 	    	process.exit(0);
 	});
 };
-/*
- commandline options are:
-	-l	lab name
-	-i	student id number
-	-h	commit hash of the student repository
-	-la	programming language
-*/
 if (argv.l && argv.i) {
 	if (argv.h) {
-		submit(argv.i, argv.l, argv.h, argv.la)
+		submit(argv.i, argv.l, argv.h);
 	}
 	else {
-		submit(argv.i, argv.l, '', argv.la);
+		submit(argv.i, argv.l, '');
 	}
 }
 else {
