b878f59f805d09bcbe88e00716ae486abb5ea28b
prasadtalasila <tsrkp@goa.bits-pilani.ac.in>
2017-03-27 10:09:33 +0530

    test all languages of lab1 using autolab.sh

--- tests/functional_tests/autolab.sh
+++ tests/functional_tests/autolab.sh
@@ -24,6 +24,11 @@ npm --quiet install 1>/dev/null
 
 #node submit.js -i 2015A7PS006G -l lab1
 node submit.js -i 2015A7PS006G -l lab1 --lang=java
+node submit.js -i 2015A7PS006G -l lab1 --lang=python2
+node submit.js -i 2015A7PS006G -l lab1 --lang=python3
+node submit.js -i 2015A7PS006G -l lab1 --lang=cpp
+node submit.js -i 2015A7PS006G -l lab1 --lang=cpp14
+node submit.js -i 2015A7PS006G -l lab1 --lang=c
 sleep 10
 #node submit.js -i 2015A7PS006G -l lab1 -la python2
 #sleep 10
