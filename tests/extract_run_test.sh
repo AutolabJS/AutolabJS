868e61b82f84e49dfce76406ad109216ccf3d166
prasadtalasila <tsrkp@goa.bits-pilani.ac.in>
2017-04-07 00:14:36 +0530

    clean up of shell scripts in tests

--- tests/extract_run_test.sh
+++ tests/extract_run_test.sh
@@ -1,21 +1,27 @@
+#!/bin/bash
+###############
+# Purpose: dummy extraction script to replace execution_nodes/extract_run.sh for testing purposes
+# Author: Ankshit Jain, Kashyap Ga
 
 #command-line variables mapped to meaningful variable names
-submission_id=$1
-lab=$2		
+submission_id="$1"
+lab="$2"
+# shellcheck disable=SC2034
 gitlab_hostname=$3	#ignored for now
+# shellcheck disable=SC2034
 commit_hash=$4		#ignored for now
 language=$5
 
 mkdir -p submissions
 cd submissions
-mkdir -p $submission_id
-cd $submission_id
-rm -rf *
+mkdir -p "$submission_id"
+cd "$submission_id"
+rm -rf ./*
 
 #create directory for the lab
-mkdir $lab
-cd $lab
+mkdir "$lab"
+cd "$lab"
 
 # copy sample unit tests and execute
 cp -rf ../../../../docs/examples/unit_tests/* .
-bash execute.sh $language
+bash execute.sh "$language"
