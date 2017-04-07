4cc9e10fddf98e2a4fc18f39f38710d146fb4662
lab_author <f2013088@goa.bits-pilani.ac.in>
2017-04-07 16:37:17 +0530

    scoreboard tests

---
+++ docs/examples/unit_tests/student_solution/java/Buyer_mistake.java
@@ -0,0 +1,32 @@
+import java.io.*;
+class Buyer
+{
+  intentional error;
+  private int cash;
+
+  public Buyer(int cash)
+  {
+    this.cash=cash;
+  }
+
+  public int getCash()
+  {
+    return cash;
+  }
+
+  public void setCash(int cash)
+  {
+    this.cash=cash;
+  }
+
+  public void buy(Seller x)
+  {
+    if(cash>0)
+    {
+      if(x.sell()==1)
+      {
+        cash--;
+      }
+    }
+  }
+}
---
+++ tests/functional_tests/worse_score1.sh
@@ -0,0 +1,6 @@
+<<<<<<< HEAD
+#!/bin/sh
+=======
+>>>>>>> add5881... scoreboard tests
+mv ../../docs/examples/unit_tests/student_solution/java/Buyer.java ../../docs/examples/unit_tests/student_solution/java/Buyer1.java
+mv ../../docs/examples/unit_tests/student_solution/java/Buyer_mistake.java ../../docs/examples/unit_tests/student_solution/java/Buyer.java
