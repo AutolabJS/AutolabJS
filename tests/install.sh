6d3affd6a6898a24a15999ad4597975717065394
Ankshit Jain <ankshitjain@gmail.com>
2017-10-06 16:32:28 +0530

    Move installation commands from test.sh to install.sh for travis

--- tests/install.sh
+++ tests/install.sh
@@ -16,3 +16,17 @@ mysql -e 'CREATE DATABASE Autolab;'
 mysql -e 'USE Autolab;'
 echo -e "USE mysql;\nUPDATE user SET password=PASSWORD('root') WHERE user='root';\nFLUSH PRIVILEGES;\n" | \
     mysql -u root
+
+npm --quiet install --prefix main_server 1>/dev/null
+npm --quiet install --prefix main_server/public/js 1>/dev/null
+npm --quiet install --prefix load_balancer 1>/dev/null
+npm --quiet install --prefix execution_nodes 1>/dev/null
+
+#copy only the necessary files to the required directories
+cp main_server/public/js/node_modules/jquery/dist/jquery.min.js main_server/public/js/
+cp main_server/public/js/node_modules/file-saver/FileSaver.min.js main_server/public/js/
+cp main_server/public/js/node_modules/materialize-css/dist/js/materialize.min.js main_server/public/js/
+cp main_server/public/js/node_modules/materialize-css/dist/css/materialize.min.css main_server/public/css/
+
+#remove the node_modules directory
+rm -rf main_server/public/js/node_modules
