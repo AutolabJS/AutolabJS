ae10dcdfc7755d17911908f6a310feedbe8cf140
Ankshit Jain <ankshitjain@gmail.com>
2017-11-07 19:18:56 +0530

    Upgrade nodejs to version 8.9 and add logger.js

--- tests/test.sh
+++ tests/test.sh
@@ -86,25 +86,25 @@ sleep 2
 # show the dependency status for all the components
 echo -e "\n\n=====Main Server Dependency Status====="
 cd ../../main_server
-npm outdated
+npm outdated || :
 npm-check || :    #bypass failure of npm-check
 
 echo -e "\n\n=====Load Balancer Dependency Status====="
 cd ../load_balancer
-npm outdated
+npm outdated || :
 npm-check || :    #bypass failure of npm-check
 
 #dependency check for execution nodes limited to one check. since all execution
 #nodes share the same initial files
 echo -e "\n\n=====Execution Nodes Dependency Status====="
 cd ../execution_nodes/execution_node_1
-npm outdated
+npm outdated || :
 npm-check || :    #bypass failure of npm-check
 cd ../
 
 echo -e "\n\n=====Functional Tests Dependency Status====="
 cd ../tests/functional_tests
-npm outdated
+npm outdated || :
 npm-check || :    #bypass failure of npm-check
 
 
