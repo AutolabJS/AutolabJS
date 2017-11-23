8b24acfe3ef4c8b5d706cc0aa937c943b5f40540
Ankshit Jain <ankshitjain@gmail.com>
2017-11-23 19:45:30 +0530

    Fix installation issues

--- docker-images/docker-pull.sh
+++ docker-images/docker-pull.sh
@@ -3,10 +3,10 @@
 # Purpose: download the required docker images for installation
 # Author: Prasad Talasila
 # Date: 16-April-2017
-# Invocation: $sudo bash docker_pull.sh
+# Invocation: $sudo bash docker-pull.sh
 # Dependencies: This script requires docker to be already installed on the system.
 ########################
 
-docker pull mysql:latest
-docker pull gitlab/gitlab-ce:latest
-docker pull ubuntu:16.04
+sudo docker pull mysql:latest
+sudo docker pull gitlab/gitlab-ce:10.1.4-ce.0
+sudo docker pull ubuntu:16.04
