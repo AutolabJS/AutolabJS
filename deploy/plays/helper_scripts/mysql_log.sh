322b2494f6b256cb5b2c919940e4d210a089b0e6
prasadtalasila <tsrkp@goa.bits-pilani.ac.in>
2018-03-25 23:37:00 +0530

    updates MySQL container configuration and the corresponding play

--- deploy/plays/helper_scripts/mysql_log.sh
+++ deploy/plays/helper_scripts/mysql_log.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-mysqlUserID=`sudo docker exec -it autolab-db id -u mysql | tr -d '\r'`
-chown -R "$mysqlUserID" ../../log/mysql/
+############
+# Purpose: Give permissions for log/mysql directory to autolab-db container
+# Date : 25-Mar-2018
+# Previous Versions: -
+# Invocation: Invoked by mysqldb.yml play; not invoked directly
+###########
+sudo chgrp -R docker ../../log/mysql/
+sudo chmod -R 775 ../../log/mysql
 mv ../configs/db/mysql.conf ../configs/db/mysql.cnf
 sudo docker restart autolab-db
