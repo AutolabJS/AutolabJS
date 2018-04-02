28bc94c410f29047abf16253dd53f4d966d78abe
Shivin Thukral <shivin.thukral7@gmail.com>
2018-04-03 03:51:43 +0530

    Update dependencies.sh

--- deploy/dependencies.sh
+++ deploy/dependencies.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
+############
+# Purpose: Installing dependencies for installation of AutoLabJS
+# Date : 03-April-2018
+# Previous Versions: -
+# Invocation: $bash dependencies.sh <inventory file>
+###########
+# All variables are in upper case convention. They are:
+#  INV_TYPE : type of inventory file for the ansible playbook. Valid values are: single_machine, two_machines.
 
-#install docker dependecy for AUFS file system
 sudo apt-get install ansible
-ansible-playbook dependencies.yml
+sudo ansible-playbook -i "$INV_TYPE" dependencies.yml
