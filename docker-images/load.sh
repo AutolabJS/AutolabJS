7817910afb68ead0a5017f47601ea5a0f4012474
Prasad Talasila <prasadtalasila@users.noreply.github.com>
2018-12-08 07:13:10 +0100

    Uses custom docker images from AutolabJS release (#314)
    The docker images from upstream providers such as Ubuntu, MySQL
      and Gitlab are continuously getting updated leading to
      installation problems.
      With a view to control the installation outcome, a custom set
      of docker images have been spun up for AutolabJS. Each new
      release of AutolabJS shall have a set of custom docker images
      to be used.

--- docker-images/load.sh
+++ docker-images/load.sh
@@ -2,14 +2,10 @@
 
 #load docker images on machine-1
 # shellcheck disable=SC2024
-sudo docker load < main_server.tar
-# shellcheck disable=SC2024
-sudo docker load < load_balancer.tar
+sudo docker load < 'nodejs.tar'
 # shellcheck disable=SC2024
 sudo docker load < 'gitlab.tar'
 # shellcheck disable=SC2024
-sudo docker load < mysql.tar
+sudo docker load < 'mysql.tar'
 # shellcheck disable=SC2024
-sudo docker load < execution_node.tar
-# shellcheck disable=SC2024
-sudo docker load < ubuntu-16.04.tar
+sudo docker load < 'execution_node.tar'
