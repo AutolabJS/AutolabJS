b734cfd6b6fc5b1c1294a4d799e97243860543dd
prasadtalasila <prasad.talasila@eng.au.dk>
2018-12-16 18:47:17 +0100

    Moves key generation script to tests
      The script for SSL key generation has been replaced with
      Ansible playbooks. The keys.sh script is only needed for tests.

--- tests/install.sh
+++ tests/install.sh
@@ -48,6 +48,7 @@ cp main_server/public/js/node_modules/file-saver/FileSaver.min.js main_server/pu
 cp main_server/public/js/node_modules/materialize-css/dist/js/materialize.min.js main_server/public/js/
 cp main_server/public/js/node_modules/materialize-css/dist/css/materialize.min.css main_server/public/css/
 cp -R tests/test_modules/data/configs deploy/.
+cp -R tests/test_modules/helper_scripts/keys/* deploy/.
 
 # remove the node_modules directory
 rm -rf main_server/public/js/node_modules
