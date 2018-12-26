5510fff732331a5c9046626faff59456569973a5
prasadtalasila <prasad.talasila@eng.au.dk>
2018-12-26 10:31:30 +0100

    Corrects the MySQL password in tests/install.sh

--- tests/install.sh
+++ tests/install.sh
@@ -25,9 +25,8 @@ fi
 # initialize the DB
 mysql -e 'CREATE DATABASE AutolabJS;'
 mysql -e 'USE AutolabJS;'
-sudo mysql -e "use mysql; update user set authentication_string=PASSWORD('new_password') \
+sudo mysql -e "use mysql; update user set authentication_string=PASSWORD('root') \
       where User='root'; update user set plugin='mysql_native_password';FLUSH PRIVILEGES;"
-sudo mysql_upgrade -u root -pnew_password
 sudo service mysql restart
 
 bash scripts/npm_install.sh travis "$(pwd)"
